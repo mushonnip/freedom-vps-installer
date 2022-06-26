9c7aebe948661568f78689fc5cd8aff3ed64ccc2
mushonnip <mushonnipabu@gmail.com>
2022-06-26 16:52:33 +0700

    update

--- README.md
+++ README.md
@@ -1,30 +1,6 @@
 # Freedom VPS Installer
 ## Services & Port
 ```
-OpenSSH                 : 443, 22
-OpenVPN                 : TCP 1194, UDP 2200, SSL 990
-Stunnel4                : 443, 222, 777
-Dropbear                : 443, 109, 143
-Squid Proxy             : 3128, 8080
-Badvpn                  : 7100, 7200, 7300
-Nginx                   : 89
-Wireguard               : 7070
-L2TP/IPSEC VPN          : 1701
-PPTP VPN                : 1732
-SSTP VPN                : 444
-Shadowsocks-R           : 1443-1543
-SS-OBFS TLS             : 2443-2543
-SS-OBFS HTTP            : 3443-3543
-XRAYS Vmess TLS         : 8443
-XRAYS Vmess None TLS    : 8880
-XRAYS Vless TLS         : 2083
-XRAYS Vless None TLS    : 2095
-XRAYS Trojan            : 2087
-Websocket TLS           : 443"
-Websocket None TLS      : 80
-Websocket Ovpn          : 2086
-OHP_SSH                 : 8181
-OHP_Dropbear            : 8282
-OHP_OpenVPN             : 8383
-Trojan Go               : 2053
+Websocket TLS           : 443
+Websocket NonTLS        : 8880
 ```
--- setup.sh
+++ setup.sh
@@ -1,144 +1,94 @@
 #!/bin/bash
-# ==============================================
-# Color
-clear
-red='\e[1;31m'
-green='\e[0;32m'
-yell='\e[1;33m'
-tyblue='\e[1;36m'
-NC='\e[0m'
-purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
-tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
-yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-# ==============================================
 
-cd /root
-
-#System version number
+# ANCHOR - Compatibility Check
 if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
 fi
 if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
+		echo "Not supported in OpenVZ virtualization"
 		exit 1
 fi
 
-localip=$(hostname -I | cut -d\  -f1)
-hst=( `hostname` )
-dart=$(cat /etc/hosts | grep -w `hostname` | awk '{print $2}')
-if [[ "$hst" != "$dart" ]]; then
-echo "$localip $(hostname)" >> /etc/hosts
-fi
+# ANCHOR - Installing Dependencies
+apt update
+apt install -y bzip2 gzip coreutils screen curl unzip
 
-echo -e "[ ${tyblue}NOTES${NC} ] Before we go.. "
-sleep 1
-echo -e "[ ${tyblue}NOTES${NC} ] I need check your headers first.."
-sleep 2
-echo -e "[ ${green}INFO${NC} ] Checking headers"
-sleep 1
-REQUIRED_PKG="linux-headers-$(uname -r)"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-  sleep 2
-  echo -e "[ ${yell}WARNING${NC} ] Try to install ...."
-  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-  apt-get --yes install $REQUIRED_PKG
-  sleep 1
-  echo ""
-  sleep 1
-  echo -e "[ ${tyblue}NOTES${NC} ] If error you need.. to do this"
-  sleep 1
-  echo ""
-  sleep 1
-  echo -e "[ ${tyblue}NOTES${NC} ] 1. apt update -y"
-  sleep 1
-  echo -e "[ ${tyblue}NOTES${NC} ] 2. apt upgrade -y"
-  sleep 1
-  echo -e "[ ${tyblue}NOTES${NC} ] 3. apt dist-upgrade -y"
-  sleep 1
-  echo -e "[ ${tyblue}NOTES${NC} ] 4. reboot"
-  sleep 1
-  echo ""
-  sleep 1
-  echo -e "[ ${tyblue}NOTES${NC} ] After rebooting"
-  sleep 1
-  echo -e "[ ${tyblue}NOTES${NC} ] Then run this script again"
-  echo -e "[ ${tyblue}NOTES${NC} ] if you understand then tap enter now"
-  read
-else
-  echo -e "[ ${green}INFO${NC} ] Okey, you have all packages"
-fi
+# ANCHOR - Setup
+# disable ipv6
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
 
-ReqPKG="linux-headers-$(uname -r)"
-if ! dpkg -s $ReqPKG  >/dev/null 2>&1; then
-  # rm /root/setup.sh >/dev/null 2>&1 
-  exit
-else
-  clear
-fi
+# load config
+. application.config
 
+# acme cert installation
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+cd /root/
+wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+bash acme.sh --install
+rm acme.sh
+cd .acme.sh
+echo "Installing Cert..."
+bash acme.sh --register-account -m $EMAIL
+bash acme.sh --issue --standalone -d $DOMAIN --force
+bash acme.sh --installcert -d $DOMAIN --fullchainpath /etc/cert/acme.crt --keypath /etc/cert/acme.key
 
-secs_to_human() {
-    echo "Installation time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minute's $(( ${1} % 60 )) seconds"
-}
-start=$(date +%s)
-ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
-sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
-sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
+# ANCHOR - All Bin Install
+chmod +x bin/*
+mv bin/ /usr/local/bin/
 
-# clear
-# if [ -f "/etc/xray/domain" ]; then
-# echo "Script Already Installed"
-# exit 0
-# fi
-# mkdir /var/lib/freedom;
-# echo "IP=" >> /var/lib/freedom/ipvps.conf
-clear
-chmod +x scripts/*.sh
+# SECTION - Websocket TLS
+# NOTE - Install Binary
+mv bin/websockets/ws-nontls /usr/local/bin/ws-tls
+chmod +x /usr/local/bin/ws-tls
 
-./scripts/cloudflare.sh > /dev/null 2>&1
-sleep 1
-clear
-screen -S ssh-vpn ./scripts/ssh-vpn.sh > /dev/null 2>&1
-sleep 1
-clear
-# screen -S xray ./xray.sh > /dev/null 2>&1
-# sleep 1
-# clear
-# screen -S sstp ./sstp.sh > /dev/null 2>&1
-# sleep 1
-# clear
-screen -S ssr ./scripts/ssr.sh > /dev/null 2>&1
-sleep 1
-clear
-# screen -S ss ./shadowsocks.sh > /dev/null 2>&1
-# sleep 1
-# clear
-# screen -S wireguard ./wireguard.sh > /dev/null 2>&1
-# sleep 1
-# clear
-# screen -S ipsec ./ipsec.sh > /dev/null 2>&1
-# sleep 1
-# clear
-# ./set-br.sh > /dev/null 2>&1
-# sleep 1
-# clear
-./scripts/websocket.sh > /dev/null 2>&1
-sleep 1
-clear
-./scripts/ohp.sh > /dev/null 2>&1
-sleep 1
-clear
+# NOTE - Install service
+cat > /etc/systemd/system/ws-tls.service << END
+[Unit]
+Description=WSS Service
+Documentation=https://google.com
+After=network.target nss-lookup.target
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 443
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target
+END
 
-chmod 644 .profile
+# !SECTION - Websocket TLS
 
-echo '============================================='
-echo '               REBOOTING SERVER'
-echo '============================================='
-echo ""
-sleep 2
-reboot
+# SECTION - Websocket Non-TLS
+# NOTE - Install Binary
+mv bin/websockets/ws-nontls /usr/local/bin/ws-nontls
+chmod +x /usr/local/bin/ws-nontls
+
+# NOTE - Install service
+cat > /etc/systemd/system/ws-nontls.service << END
+[Unit]
+Description=WS Service
+Documentation=https://google.com
+After=network.target nss-lookup.target
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 8880
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target
+END
+
+# !SECTION - Websocket Non-TLS
+
+# ANCHOR - Enable Services
+systemctl daemon-reload
+systemctl enable ws-tls --now
+systemctl enable ws-nontls --now
